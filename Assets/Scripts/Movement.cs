86566cf7517bc4aa4b4fbb7bf2996810f017dff7
Brian Clanton <brian.clanton@gmail.com>
2021-01-29 22:25:22 -0800

    Basic movement and added first real asset

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 1f;
+    private float xInput;
+    private Rigidbody2D rigidBody;
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        rigidBody.velocity = new Vector2(xInput * speed, rigidBody.velocity.y);
+    }
+}
