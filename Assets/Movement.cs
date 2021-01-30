6ff428b7f713158859853703636d60aad1fbcb97
Brian Clanton <brian.clanton@gmail.com>
2021-01-29 20:40:27 -0800

    Initial commit

---
+++ Assets/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = .1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+
+        Vector3 moveDirection = new Vector3(x, 0, 0);
+
+        transform.position += moveDirection * speed;
+    }
+}
