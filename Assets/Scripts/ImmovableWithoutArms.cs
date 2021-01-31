de0e407fb72cad3bb3e2446a3c8f009deb7cc7ff
Brian Clanton <brian.clanton@gmail.com>
2021-01-30 21:11:19 -0800

    Block is only movable if you have arms

---
+++ Assets/Scripts/ImmovableWithoutArms.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ImmovableWithoutArms : MonoBehaviour
+{
+    private Inventory inventory;
+    private Rigidbody2D rigidBody;
+
+    public void Awake()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+        inventory.OnChangedEvent.AddListener(UpdateMass);
+
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void UpdateMass() {
+        rigidBody.mass = inventory.HasPart(BodyPartType.Arm) ? 1 : 100;
+    }
+}
