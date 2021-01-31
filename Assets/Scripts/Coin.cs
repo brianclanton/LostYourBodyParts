6070190a6eb686b674244eb20b0bf51e2a0796f5
Brian Clanton <brian.clanton@gmail.com>
2021-01-31 08:41:23 -0800

    Added coin script

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private bool canPickup;
+
+    void Update()
+    {
+        if (canPickup && Input.GetKeyDown(KeyCode.E))
+        {
+            // TODO: Apply coin to player
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            canPickup = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            canPickup = false;
+        }
+    }
+}
