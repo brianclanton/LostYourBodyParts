3034e82d7e920559142b4d0616105df75ce7bf46
Brian Clanton <brian.clanton@gmail.com>
2021-01-31 13:21:53 -0800

    Setup narration prefab

--- Assets/Scripts/Collectibles.cs
+++ Assets/Scripts/Collectibles.cs
@@ -1,9 +1,14 @@
-﻿public static class Collectibles
+﻿using UnityEngine;
+
+public static class Collectibles
 {
     public static int count { get; private set; }
 
     public static void AddCollectible()
     {
+        var narrationController = GameObject.FindGameObjectWithTag("Narration").GetComponent<NarrationController>();
+        narrationController.PlayNarration(count);
+
         count++;
     }
 }
---
+++ Assets/Scripts/NarrationController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class NarrationController : MonoBehaviour
+{
+    public AudioClip[] audioClips;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayNarration(int num)
+    {
+        if (audioSource.isPlaying)
+        {
+            audioSource.Pause();
+        }
+
+        audioSource.clip = audioClips[num];
+        audioSource.Play();
+    }
+}
