a06e3009dbd53cdfa4d10e8db992167222d6ca88
Brian Clanton <brian.clanton@gmail.com>
2021-01-31 10:04:41 -0800

    Created level prefab

---
+++ Assets/Scripts/Lever.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    private bool canOpen;
+
+    void Update()
+    {
+        if (canOpen && Input.GetKeyDown(KeyCode.E))
+        {
+            foreach (GameObject lockedDoor in GameObject.FindGameObjectsWithTag(tag))
+            {
+                if (lockedDoor != gameObject)
+                {
+                    Destroy(lockedDoor);
+                }
+            }
+
+            Debug.Log("Rotated");
+            var leverArm = transform.Find("lever-p");
+            var pivotPoint = leverArm.transform.Find("pivot").transform.position;
+            leverArm.transform.RotateAround(transform.position, 45f);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            canOpen = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            canOpen = false;
+        }
+    }
+}
