e1ba2d2edb8cd3a6d97e5feb129eb7b489dd25d1
Deshawn Cervi <deshawn.cervi@gmail.com>
2021-01-31 15:28:25 -0700

    Moving Platform Prefab

---
+++ Assets/MovingPlatform.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public GameObject startpoint;
+    public GameObject endpoint;
+    public GameObject temp;
+    public float velocity = 4f;
+    public bool active = true;
+    public int direction = 1;
+
+    void FixedUpdate()
+    {
+        if (active)
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y + (velocity * Time.fixedDeltaTime * direction));
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == endpoint) 
+        {
+            direction = -1;
+        }        
+        if (collision.gameObject == startpoint)
+        {
+            direction = 1;
+        }
+    }
+
+
+}
