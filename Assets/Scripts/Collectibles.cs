59841b95ef14657fafae10932aeb2c5e0bce0ae8
Brian Clanton <brian.clanton@gmail.com>
2021-01-31 13:49:20 -0800

    Added reset handler to all levels

--- Assets/Scripts/Collectibles.cs
+++ Assets/Scripts/Collectibles.cs
@@ -3,9 +3,20 @@
 public static class Collectibles
 {
     public static int count { get; private set; }
+    public static int levelStartCount { get; private set; }
 
     public static void AddCollectible()
     {
         count++;
+    }
+
+    public static void UpdateLevelStartCount()
+    {
+        levelStartCount = count;
+    }
+
+    public static void ResetCount()
+    {
+        count = levelStartCount;
     }
 }
