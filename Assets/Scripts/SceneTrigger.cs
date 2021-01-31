43fd74da5a3b8746d709f0a3af62a6f2b62656aa
khoaxial <54433234+khoaxial@users.noreply.github.com>
2021-01-31 14:59:00 -0500

    Add transition between 1 and 2

---
+++ Assets/Scripts/SceneTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTrigger : MonoBehaviour
+{
+    public string targetScene;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            SceneManager.LoadScene(targetScene);
+    }
+}
